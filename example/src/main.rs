888e8b51a0d8e12e81b471903c5b486f516db2f9
poccariswet <s1240231@gmail.com>
2019-09-30 01:58:11 +0900

    [FIX] apng encoder mod lib

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,29 +1,33 @@
 use apng;
+use apng::Encoder;
+use apng::{Frame, PNGImage, APNG};
 use std::fs::File;
 
 fn main() {
-    println!("let's go apng!");
-
     let files = vec![
-        "sample/ouija1.png",
-        "sample/ouija2.png",
-        "sample/ouija3.png",
-        "sample/ouija4.png",
+        "sample/logo1.png",
+        "sample/logo2.png",
+        "sample/logo3.png",
+        //"sample/logo4.png",
+        //"sample/logo5.png",
+        //"sample/logo6.png",
+        //"sample/logo7.png",
+        //"sample/logo8.png",
     ];
 
     let mut pngs: Vec<PNGImage> = Vec::new();
     for f in files.iter() {
-        pngs.push(load_png(f).unwrap());
+        pngs.push(apng::load_png(f).unwrap());
     }
 
-    let mut out = File::create("sample/test.png").unwrap();
+    let mut out = File::create("sample/out.png").unwrap();
     let mut apng = APNG { images: pngs };
     let mut encoder = Encoder::new(&mut out, apng.create_config(0).unwrap()).unwrap();
-    let _frame = Frame {
+    let frame = Frame {
         delay_num: Some(1),
-        delay_den: Some(5),
+        delay_den: Some(2),
         ..Default::default()
     };
-    let err = encoder.encode_all(apng);
+    let err = encoder.encode_all(apng, Some(&frame));
     println!("{:?}", err)
 }
