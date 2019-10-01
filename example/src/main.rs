7ace50df4d78ee525275b5df201f1bf3c1135925
poccariswet <s1240231@gmail.com>
2019-10-02 00:41:51 +0900

    [ADD] example main

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,15 +2,17 @@ use apng;
 use apng::Encoder;
 use apng::{Frame, PNGImage, APNG};
 use std::fs::File;
+use std::io::{BufWriter, Read, Write};
+use std::path::Path;
 
 fn main() {
     let files = vec![
         "sample/rust_logo1.png",
         "sample/rust_logo2.png",
-        "sample/rust_logo3.png",
-        "sample/rust_logo4.png",
-        "sample/rust_logo5.png",
-        "sample/rust_logo6.png",
+        //"sample/rust_logo3.png",
+        //"sample/rust_logo4.png",
+        //"sample/rust_logo5.png",
+        //"sample/rust_logo6.png",
     ];
 
     let mut pngs: Vec<PNGImage> = Vec::new();
@@ -18,7 +20,10 @@ fn main() {
         pngs.push(apng::load_png(f).unwrap());
     }
 
-    let mut out = File::create("sample/out.png").unwrap();
+    let path = Path::new(r"sample/out.png");
+    let mut out_file = File::create(path).unwrap();
+    let mut out = BufWriter::new(out_file);
+
     let mut apng = APNG { images: pngs };
     let mut encoder = Encoder::new(&mut out, apng.create_config(0).unwrap()).unwrap();
     let frame = Frame {
