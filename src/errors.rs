d4622de099f6125bd25be993eb2dfef0307e898e
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2021-07-08 04:48:44 +0300

    [REFACTOR] use `thiserror` instead of `failure` (#21)
    failure is deprecated: https://github.com/rust-lang-nursery/failure/pull/347

--- src/errors.rs
+++ src/errors.rs
@@ -1,48 +1,24 @@
-use failure::Fail;
 use std::io::Error as IOError;
+use thiserror::Error as ThisError;
 
 pub type APNGResult<T> = Result<T, APNGError>;
 
-#[derive(Fail, Debug)]
+#[derive(ThisError, Debug)]
 pub enum APNGError {
-    #[fail(display = "IO error: {}", 0)]
-    Io(IOError),
-    #[fail(display = "images are not found")]
+    #[error("IO error: {0}")]
+    Io(#[from] IOError),
+    #[error("images are not found")]
     ImagesNotFound,
-    #[fail(display = "wrong data size, expected {} got {}", 0, 1)]
+    #[error("wrong data size, expected {0} got {1}")]
     WrongDataSize(usize, usize),
-    #[fail(display = "wrong frames nums, expected {} got {}", 0, 1)]
+    #[error("wrong frames nums, expected {0} got {1}")]
     WrongFrameNums(usize, usize),
 }
 
-macro_rules! define_error {
-    ($source:ty, $kind:tt) => {
-        impl From<$source> for APNGError {
-            fn from(error: $source) -> APNGError {
-                APNGError::$kind(error)
-            }
-        }
-    };
-}
-
-define_error!(std::io::Error, Io);
-
 pub type AppResult<T> = Result<T, AppError>;
 
-#[derive(Fail, Debug)]
+#[derive(ThisError, Debug)]
 pub enum AppError {
-    #[fail(display = "png decode error: {}", 0)]
+    #[error("png decode error: {0}")]
     PNGImage(png::DecodingError),
 }
-
-macro_rules! define_error {
-    ($source:ty, $kind:ident) => {
-        impl From<$source> for AppError {
-            fn from(error: $source) -> AppError {
-                AppError::$kind(error)
-            }
-        }
-    };
-}
-
-define_error!(png::DecodingError, PNGImage);
